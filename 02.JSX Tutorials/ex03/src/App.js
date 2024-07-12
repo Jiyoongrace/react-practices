6e90bb857c2d815fd8cc05f916f77e5e51720a0d
Jiyoongrace <giyun3632@naver.com>
2024-07-12 13:34:28 +0900

    :sparkles: feat: 함수 컴포넌트

---
+++ 02.JSX Tutorials/ex03/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from "./Header";
+import Contents from "./Content";
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <Contents />
+        </div>
+    );
+}
+
+export {App};
