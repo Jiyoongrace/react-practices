e542f1049d4cd4806a193fb6d5d958a6f47ac915
Jiyoongrace <giyun3632@naver.com>
2024-07-13 20:28:30 +0900

    :sparkles: feat: tabbox

---
+++ tabbox/frontend/src/TabView.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Tab_View} from './assets/scss/TabView.scss';
+
+function TabView(props) {
+    return (
+        <div className={Tab_View}>
+            <p>탭뷰입니다.</p>
+        </div>
+    );
+}
+
+export default TabView;
