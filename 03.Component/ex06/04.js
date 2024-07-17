c505043b7eed901ab8f8b547eb630dcb88790e06
Jiyoongrace <giyun3632@naver.com>
2024-07-17 11:20:37 +0900

    :sparkles: feat: Immutability(불변성) of Component's State - [react-addons-update]

--- 03.Component/ex06/04.js
+++ 03.Component/ex06/04.js
@@ -2,7 +2,34 @@ import fs from 'fs';
 import update from 'react-addons-update';
 
 console.log("== Sol =========================================");
-let state = {
-    order: JSON.parse(fs.readFileSync('./json/data.json', 'utf-8'))
-};
+const order = JSON.parse(fs.readFileSync('./json/data.json', 'utf-8'));
 
+const updateOrder = update(order, {
+    // 프로퍼티 변경
+    receive: {
+        $set: "강남구 논현동..."
+    },
+    // Nest 객체 프로퍼티 변경
+    payment: {
+        method: {
+            $set: "Mobile"
+        }
+    },
+    products: {
+        // 배열 요소 객체 프로퍼티 변경
+        0: {
+            amount: {
+                $set: 200
+            }
+        },
+        // 배열 요소 추가
+        $push: [{
+            no: 'c002-006',
+            name: '핑크 양말',
+            price: 3000,
+            amount: 5
+        }]
+    }
+});
+
+console.log(order, updateOrder);
