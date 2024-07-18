89b9ac57d2f045ef160b53b32d74feaffa71c9e4
Jiyoongrace <giyun3632@naver.com>
2024-07-18 15:40:23 +0900

    :zap: feat: kanbanboard api

---
+++ kanbanboard/backend/src/main/java/com/poscodx/kanbanboard/dto/JsonResult.java
@@ -0,0 +1,30 @@
+package com.poscodx.kanbanboard.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class JsonResult {
+	private String result;	//"success" or "fail"
+	private Object data;	// if success, set! 
+	private String message; // if fail, set!
+	
+	private JsonResult(Object data) {
+		this.result = "success";
+		this.data = data;
+	}
+
+	private JsonResult(String message) {
+		this.result = "fail";
+		this.message = message;
+	}
+
+	public static JsonResult success(Object data) {
+		return new JsonResult(data);
+	}
+	
+	public static JsonResult fail(String message) {
+		return new JsonResult(message);
+	}
+}
