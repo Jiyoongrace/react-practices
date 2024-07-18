635bff21febaac43053a7fdfe1de121d12fd5520
Jiyoongrace <giyun3632@naver.com>
2024-07-18 11:33:57 +0900

    :art: fix: scss

--- emaillist08/frontend/src/Email.js
+++ emaillist08/frontend/src/Email.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {_Email} from './assets/scss/Email.scss';
+import styles from './assets/scss/Email.scss';
 
 function Email({firstName, lastName, email}) {
     return (
-        <li className={_Email}>
-            {firstName}{lastName}
-            <br/>
-            {email}
+        <li className={styles._Email}>
+            <h4>{firstName}{lastName}</h4>
+            <span>{email}</span>
+            <a href='' />
         </li>
     );
 }
