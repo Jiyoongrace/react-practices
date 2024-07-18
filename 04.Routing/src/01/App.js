fd30d7d708b8fc77d8930ee64640cbebb7fb1eb8
Jiyoongrace <giyun3632@naver.com>
2024-07-18 16:26:41 +0900

    :sparkles: feat: Routing - [React Router Basic I: Implemented in a Simple Way(hashchange event driven)]

--- 04.Routing/src/01/App.js
+++ 04.Routing/src/01/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Main from "./component/Main";
 import Gallery from "./component/Gallery";
 import Guestbook from "./component/Guestbook";
@@ -7,7 +7,18 @@ import Error404 from "./component/Error404";
 export default function App() {
     const [route, setRoute] = useState('');
 
+    const hadlerHashChage = () => {
+        console.log(window.location.hash);
+        setRoute(window.location.hash.substring(1));
+    }
 
+    useEffect(() => {
+        window.addEventListener("hashchange", hadlerHashChage);
+
+        return () => {
+            window.removeEventListener("hashchange", hadlerHashChage);
+        }
+    }, []);
 
     return (() => {
         switch(route) {
@@ -19,7 +30,7 @@ export default function App() {
             case '/gallery':
                 return <Gallery />;
             default :
-                return <Error404 />;                    
+                return <Error404 />;
         }
     })();
 }
